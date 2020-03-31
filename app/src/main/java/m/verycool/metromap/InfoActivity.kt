8b39a2797cc5b69b30bfe2aa0c805693e9178807
Aquatoriya <aquatoriya@bk.ru>
2020-03-31 18:15:03 +0300

    Previous version of parser vol.3

--- app/src/main/java/m/verycool/metromap/InfoActivity.kt
+++ app/src/main/java/m/verycool/metromap/InfoActivity.kt
@@ -1,11 +1,17 @@
 package m.verycool.metromap
 
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import m.verycool.metromap.model.MetroTitle
 import org.w3c.dom.Element
 import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import org.w3c.dom.Document
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 
@@ -22,53 +28,42 @@ class InfoActivity : AppCompatActivity() {
         val stationTitle = intent.getSerializableExtra(MetroTitle::class.java.simpleName)
         Toast.makeText(this, stationTitle.toString(), Toast.LENGTH_LONG).show()
 
+        fun main(args: Array<String>) {
+            val xlmFile: File =
+                File("C:\\Users\\User\\AndroidStudioProjects\\MetroMap\\app\\src\\main\\res\\layout\\db_handler.xml")
+            val xmlDoc: Document =
+                DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xlmFile)
 
-//        fun stationList(name: String) : Boolean {
-//            val lv = findViewById<ListView>(R.id.listView)
-//            val xmlFile = File("db_handler.xml")
-//            val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
-//            var list = doc.getElementsByTagName("station")
-//            for (i in 0 until list.length) {
-//                if (list.item(0).getNodeType().equals(Node.ELEMENT_NODE) ) {
-//                    empList.clear()
-//                    //creating instance of HashMap to put the data of node value
-//                    empDataHashMap = HashMap()
-//                    val element = list.item(i) as Element
-//                    empDataHashMap.put("name", getNodeValue("name", element))
-//                    empDataHashMap.put("date", getNodeValue("date", element))
-//                    empDataHashMap.put("depth", getNodeValue("depth", element))
-//                    empDataHashMap.put("architects", getNodeValue("architects", element))
-//                    //adding the HashMap data to ArrayList
-//                    empList.add(empDataHashMap)
-//
-//
-//                    val adapter = SimpleAdapter(this@InfoActivity, empList, R.layout.info_activity, arrayOf("name", "date", "depth", "architects"), intArrayOf(R.id.name, R.id.date, R.id.depth, R.id.architects))
-//                    lv.setAdapter(adapter)
-//
-//                }
-//            }
-//            return false
-//        }
-//
-    }
+            xmlDoc.documentElement.normalize()
+
+            val stationList: NodeList = xmlDoc.getElementsByTagName("station")
+
+            for (i in 0..stationList.length - 1) {
+                var stationNode: Node = stationList.item(i)
+
+                if (stationNode.getNodeType() === Node.ELEMENT_NODE) {
+
+                    val elem = stationNode as Element
 
 
-    // function to return node value
-    protected fun getNodeValue(tag: String, element: Element): String {
-        val nodeList = element.getElementsByTagName(tag)
-        val node = nodeList.item(0)
-        if (node != null) {
-            if (node.hasChildNodes()) {
-                val child = node.getFirstChild()
-                while (child != null) {
-                    if (child.getNodeType() === Node.TEXT_NODE) {
-                        return child.getNodeValue()
+                    val mMap = mutableMapOf<String, String>()
+
+
+                    for (j in 0..elem.attributes.length - 1) {
+                        mMap.putIfAbsent(
+                            elem.attributes.item(j).nodeName,
+                            elem.attributes.item(j).nodeValue
+                        )
                     }
+                    println("Current station : ${stationNode.nodeName}")
+
+                    println("Имя: ${elem.getElementsByTagName("name").item(0).textContent}")
+                    println("Дата: ${elem.getElementsByTagName("date").item(0).textContent}")
+                    println("Глубина: ${elem.getElementsByTagName("depth").item(0).textContent}")
+                    println("Архитекторы: ${elem.getElementsByTagName("architects").item(0).textContent}")
+                    println("История: ${elem.getElementsByTagName("history").item(0).textContent}")
                 }
             }
         }
-        return ""
-
-
-    }
+        }
 }
